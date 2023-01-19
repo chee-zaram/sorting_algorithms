211b004dc1a74252c9c3b77fb39a6f7d7165183e
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2023-01-19 22:11:14 +0100

    refactor: make `insertion sort` algo `betty` compliant

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,95 +1,46 @@
 #include "sort.h"
-#include <stdlib.h>
 
 /**
- * insertion_sort - Sorts a doubly linked list of integers in ascending order
- * @list: Address of the head of the list
+ * insertion_sort_list - Implementation of insertion sort algorithm in dll
+ * @list: Address to the start of the list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node_to_insert = NULL, *next_to_insert = NULL;
-	listint_t  *new_last, *new_prev, *new_next, *old_next;
+	listint_t *current, *current_prev;
 
-	if (!list || !*list)
+	if (!list || !*list || !(*list)->next)
 		return;
 
-	if (!(*list)->next)
-		return;
-
-	if (!(*list)->next->next)
+	current = (*list)->next;
+	while (current)
 	{
-		if ((*list)->next->n < (*list)->n)
-		{
-			(*list)->next->prev = NULL;
-			(*list)->next->next = *list;
-			(*list)->prev = (*list)->next;
-			*list = (*list)->next;
-			(*list)->next->next = NULL;
-			print_list(*list);
-		}
-		return;
+		current_prev = current->prev;
+		insertion_sort(list, current, current_prev);
+		current = current->next;
 	}
+}
 
-	if ((*list)->next->n < (*list)->n)
+/**
+ * insertion_sort - Sorts the doubly linked list
+ * @list: Address to the head of the list
+ * @current: Current item to sort
+ * @curr_prev: Item that comes before the current one on the list
+ */
+void insertion_sort(listint_t **list, listint_t *current, listint_t *curr_prev)
+{
+	while (curr_prev && curr_prev->n > current->n)
 	{
-		(*list)->next->prev = NULL;
-		(*list)->next->next = *list;
-		(*list)->prev = (*list)->next;
-		*list = (*list)->next;
-		print_list(*list);
-	}
-
-	node_to_insert = (*list)->next->next;
-	next_to_insert = node_to_insert->next;
-
-	while (node_to_insert)
-	{
-		if (!node_to_insert->next && node_to_insert->prev && node_to_insert->prev->prev)
-		{
-			if (node_to_insert->prev->n <= node_to_insert->n)
-				break;
-
-			new_prev = node_to_insert->prev->prev;
-			new_last = node_to_insert->prev;
-
-			new_last->next = NULL;
-			new_last->prev = node_to_insert;
-			new_prev->next = node_to_insert;
-			node_to_insert->prev = new_prev;
-			node_to_insert->next = new_last;
-			print_list(*list);
-
-			if (node_to_insert->prev && node_to_insert->prev->n <= node_to_insert->n)
-				break;
-		}
-
-		else if (node_to_insert->prev && node_to_insert->prev->n > node_to_insert->n)
-		{
-			new_prev = node_to_insert->prev->prev;
-			new_next = node_to_insert->prev;
-			old_next = node_to_insert->next;
-
-			old_next->prev = new_next;
-			new_next->next = old_next;
-			new_next->prev = node_to_insert;
-			node_to_insert->next = new_next;
-			if (new_prev)
-			{
-				new_prev->next = node_to_insert;
-				node_to_insert->prev = new_prev;
-			}
-			else
-			{
-				node_to_insert->prev = NULL;
-				*list = node_to_insert;
-			}
-			print_list(*list);
-		}
+		curr_prev->next = current->next;
+		if (current->next)
+			current->next->prev = curr_prev;
+		current->prev = curr_prev->prev;
+		if (curr_prev->prev)
+			curr_prev->prev->next = current;
 		else
-		{
-			node_to_insert = next_to_insert;
-			if (next_to_insert && next_to_insert->next)
-				next_to_insert = next_to_insert->next;
-		}
+			*list = current;
+		curr_prev->prev = current;
+		current->next = curr_prev;
+		print_list(*list);
+		curr_prev = current->prev;
 	}
 }
--- sort.h
+++ sort.h
@@ -4,6 +4,12 @@
 #define FALSE	0
 #define TRUE	1
 
+#define CHECK_FOR_LAST_NODE	\
+(!node_to_insert->next && node_to_insert->prev && node_to_insert->prev->prev)
+
+#define CHECK_FOR_REGULAR	\
+(node_to_insert->prev && node_to_insert->prev->n > node_to_insert->n)
+
 #include <stdio.h>
 
 /**
@@ -24,6 +30,11 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-listint_t *find_loop(listint_t *head);
+void insertion_sort(listint_t **list, listint_t *current, listint_t *cur_prev);
+
+/* void sort_list_of_two_and_print(listint_t **list); */
+/* void sort_two_only(listint_t **list); */
+/* int sort_for_last_node(listint_t *node_to_insert); */
+/* void sort_for_regular(listint_t **list, listint_t *node_to_insert); */
 
 #endif /* for _SORT_H_ */
